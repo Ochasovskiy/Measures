c4e36a7747f7fde4385f501498c3b21bac3dbf36
Oleksandr <alexander.chasovskiy@gmai.com>
2026-08-07 12:04:44 +0200

    Initial Commit

---
+++ MeasureGoTests/MeasureGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureGoTests.swift
+//  MeasureGoTests
+//
+//  Created by Alexander on 07.08.2026.
+//
+
+import Testing
+@testable import MeasureGo
+
+struct MeasureGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
